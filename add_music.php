52099f7df8be28660299a8ef5d1bc8f8d568d35d
Christopher CUEILLE <kriis958@hotmail.fr>
2015-10-05 18:55:00 +0200

    Fix #3

--- add_music.php
+++ add_music.php
@@ -2,27 +2,27 @@
 session_start();
 require('config/config.php');
 require('model/functions.fn.php');
-
 if( isset($_FILES['music']) && !empty($_FILES['music']) && 
 	isset($_POST['title']) && !empty($_POST['title'])){
 	
 	$file = $_FILES['music'];
-
-	// Si le "fichier" reçu est bien un fichier
+	$title = $_POST['title'];
+	$user_id = $_SESSION['id'];
+	var_dump($user_id);
+	if(is_file($file) === true ) {
 		$ext = strtolower(substr(strrchr($file['name'], '.')  ,1));
 		// Vérification des extentions
 		if (preg_match('/\.(mp3|ogg)$/i', $file['name'])) {
 			$filename = md5(uniqid(rand(), true));
 			$destination = "musics/{$filename}.{$_SESSION['id']}.{$ext}";
-
-			// TODO
-
+			move_uploaded_file($file, $destination);
+			addMusic($db, $user_id, $title, $file);
+			header('Location: dashboard.php');
 		} else {
 			$error = 'Erreur, le fichier n\'a pas une extension autorisée !';
 		}
-	// }
+	 }
 }
-
 include 'view/_header.php';
 include 'view/add_music.php';
 include 'view/_footer.php';
